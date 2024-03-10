28b12676b606e037ff8481c02dbb2aa606bf2951
Soujanya Bhowmick <soujanyawork600@gmail.com>
2024-03-10 14:02:29 +0200

    2d vector practice code

---
+++ Day7_CPP/17_2d_arr.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<vector<int>> array1(const vector<vector<int>> &arr)
+{
+}
+
+void array2(const vector<vector<int>> &arr)
+{
+    // Your code here
+    // No return statement needed for a void function.
+    for (int i = 0; i < 1; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void array3(const vector<vector<int>> &arr)
+{
+    // Your code here
+    // No return statement needed for a void function.
+    for (int i = 2; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void array4(const vector<vector<int>> &arr)
+{
+    // Your code here
+    // No return statement needed for a void function.
+    for (int i = 1; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    vector<vector<int>> arr = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+
+    };
+    // cout << arr[1][2] << endl;
+    array2(arr);
+    array3(arr);
+    array4(arr);
+    return 0;
+}
