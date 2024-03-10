9a1cd8734972ccca599f4578ebf2b65c45923564
Soujanya Bhowmick <soujanyawork600@gmail.com>
2024-03-10 15:43:27 +0200

    2d vector practice code 19_2d_arr.cpp

---
+++ Day7_CPP/19_2d_arr.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool searchElement(const std::vector<std::vector<int>> &array, int target)
+{
+    for (size_t i = 0; i < array.size(); ++i)
+    { // Iterate over each row
+        for (size_t j = 0; j < array[i].size(); ++j)
+        { // Iterate over each column in the current row
+            if (array[i][j] == target)
+            {
+                // Element found
+                return true;
+            }
+        }
+    }
+    // Element not found
+    return false;
+}
+int main()
+{
+    cout << " element search in array" << endl;
+    vector<vector<int>> array1 = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}};
+
+    cout << searchElement(array1, 10) << endl;
+    return 0;
+}
