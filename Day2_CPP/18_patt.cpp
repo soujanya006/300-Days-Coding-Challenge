ea410adb4543fe5d358b6b17a9c943daae2cbf68
Soujanya Bhowmick <soujanyawork600@gmail.com>
2024-04-15 11:38:08 +0300

    modified

--- Day2_CPP/18_patt.cpp
+++ Day2_CPP/18_patt.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
 
-    int row = 10;
-    for(int i = 0 ; i < row; i++){
-        for(int j = 0 ; j < i + 1; j++){
+    int row = 7;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
             cout << "* ";
         }
-    cout << endl;
+        cout << endl;
     }
     return 0;
 }
--- Day2_CPP/19_patt.cpp
+++ Day2_CPP/19_patt.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
+
     int row = 5;
-    for (int i = 0; i < row; i++){
-        for(int j = 0; j < row-i; j++){
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < row - i; j++)
+        {
             cout << "* ";
         }
-    cout << endl ; 
+        cout << endl;
     }
-    return 0; 
-
+    return 0;
 }
